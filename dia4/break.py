f9fa19d325d77fc0a2c40da1b6792ed57f196040
Marioneta52 <mario-2587@hotmail.com>
2025-08-22 12:00:03 -0500

    HPupdta01

---
+++ dia4/break.py
@@ -0,0 +1,8 @@
+
+
+#break
+nombre = input("Ingresa una nombre")
+for letra in nombre:
+    if letra == 'a':
+        print(letra)
+        break
---
+++ dia4/look_while.py
@@ -0,0 +1,36 @@
+#rango no incluye el 5
+lista = list(range(10,1001))
+print(lista)
+for numero in range(5):
+        print(numero)
+
+
+#
+monedas = 5
+while monedas > 0:
+    print(f"tengo {monedas} monedas")
+    monedas -= 1
+else:print("No tengo más monedas")
+
+
+respuesta = 's'
+while respuesta == 's':
+    respuesta= input("Ingresa una respuesta (s/n)")
+else:
+    print('Gracias')
+
+#pass
+respuesta2 = 'n'
+while respuesta2 == 's':
+    pass
+print('Gracias2')
+
+
+#break - continiu
+nombre = input("Ingresa una nombre")
+for letra in nombre:
+    if letra == 'a':
+        print(letra)
+        break
+
+

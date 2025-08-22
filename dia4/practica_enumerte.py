eeb6eea3f600bfc0c0ccf2b181ddb181dc647974
John Agudelo <mario-2587@hotmail.com>
2025-08-22 16:16:14 -0500

    LenovoUp01

---
+++ dia4/practica_enumerte.py
@@ -0,0 +1,14 @@
+#Imprime en pantalla únicamente los índices de aquellos nombres de la lista a continuación, que empiecen con M:
+lista_nombres = ["Marcos", "Laura", "Mónica", "Javier", "Celina", "Marta", "Darío", "Emiliano", "Melisa"]
+for indice, nombre in enumerate(lista_nombres):
+    if nombre[0] == 'M':
+        print(indice)
+
+lista_nombres = ["Marcos", "Laura", "Mónica", "Javier", "Celina", "Marta", "Darío", "Emiliano", "Melisa"]
+for  indice, nombre in enumerate(lista_nombres):
+    print(f'{nombre} se encuentra en el índice {indice}')
+
+
+
+
+

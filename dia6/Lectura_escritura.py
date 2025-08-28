104ba8d29cebdb506d5930069607262388e5d4b7
Marioneta52 <mario-2587@hotmail.com>
2025-08-28 10:41:26 -0500

    HPupdta07

--- dia6/Lectura_escritura.py
+++ dia6/Lectura_escritura.py
@@ -3,6 +3,6 @@
 # 'w' si ya existe el archivo se vacia, si no lo crea
 # 'a' Si el archivo no existe lo crea, de lo contrario se posiciona al final y guarda lo que ya existe
 
-archivo = open('file01.txt','w')
-archivo.write('soy el nuevo texto')
+archivo = open('file02.txt','w')
+archivo.writelines(['soy el nuevo texto', 'hola','mundo','aqui', 'estoy'])
 archivo.close()

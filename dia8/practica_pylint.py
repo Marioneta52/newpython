160c702683f730c6b8c5faf68e80c3528b5d845d
John Agudelo <mario-2587@hotmail.com>
2025-09-08 09:27:50 -0500

    LenovoUp07

---
+++ dia8/practica_pylint.py
@@ -0,0 +1,16 @@
+'''Este es un modulo de ejemplo arreglo pylint
+error C0303 espacio en blanco al final de la linea borrar espacios'''
+
+
+def Sumar(número1, número2):
+
+    '''Esta función hace algo interesante con los dos parámetros.'''
+
+
+    return número1+número2
+
+
+suma = Sumar(5,7)
+
+
+print(f'El resultado de la suma fue: {suma}')

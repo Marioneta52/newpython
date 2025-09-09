09ca712bc5e61bede22d936b2ea55f9176ef192b
Marioneta52 <mario-2587@hotmail.com>
2025-09-09 12:14:18 -0500

    HPupdta07

--- dia9/OS_Shutil.py
+++ dia9/OS_Shutil.py
@@ -8,16 +8,26 @@ print(os.getcwd())
 
 #Crear archivo
 archivo = open('curso.txt','w')
-archivo.write('Texto Prueba')
+#archivo.write('Texto Prueba')
 archivo.close()
 
 print(os.listdir())
 
 #Shutil me permite mover
-shutil.move('curso.txt','/home/elitebook/Escritorio')
+#shutil.move('curso.txt','/home/elitebook/Escritorio')
 
 #Eliminar archivos OS.UN elimina archivo de una ruta OS.RM elimina carpeta vacia
 #shutil.rmt eliminar todo dentro de una ruta
 
-send2trash.send2trash('curso.txt')
+ruta    ='/home/elitebook/Documentos/Python_Workspace/python/dia8'
 
+
+for carpeta, subcarpeta, archivo in os.walk(ruta):
+    print(f'En la carpeta {ruta}')
+    print(f'En la subdirs {subcarpeta}')
+    for sub in  subcarpeta:
+        print(f'\t{sub}')
+    print('Los archivos son:')
+    for arch in archivo:
+        print(f'\t{arch}')
+    print('/n')
